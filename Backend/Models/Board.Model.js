9209eb216ad3ee5d7819c911b203066b63058be1
Saurabh209 <saurabhhh2613@gmail.com>
2025-12-09 21:39:10 +0530

    minor change in board model

--- Backend/Models/Board.Model.js
+++ Backend/Models/Board.Model.js
@@ -5,7 +5,7 @@ const boardSchema = new mongoose.Schema(
         name: {
             type: String,
             required: true,
-            unique: true
+           
         },
         password: {
             type: String,
